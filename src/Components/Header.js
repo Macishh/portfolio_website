706ad62ba155543e46c2cbe7f9f84ab2b5b4a563
Macish <marcusaxelsson52@gmail.com>
2020-04-05 18:45:36 +0200

    Added start page picture and text

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,7 +5,23 @@ class Header extends Component {
   render() {
     return (
       <div>
-        <h1>{data.name}</h1>
+        <img
+          className="header-logo"
+          src={require("../img/marcus_axelsson_logo.png")}
+          alt={data.name}
+        ></img>
+        <div className="start-page">
+          <img
+            className="start-page-marcus"
+            src={require("../img/start_page_marcus.png")}
+            alt={"Start page pic"}
+          ></img>
+          <h1 className="header-h1">
+            Hi, my name is
+            <br></br>
+            Marcus Axelsson
+          </h1>
+        </div>
       </div>
     );
   }
--- src/mydata.js
+++ src/mydata.js
@@ -1,3 +1,4 @@
 export default {
   name: "Marcus Axelsson",
+  logo: "./img/marcus_axelsson_logo.png",
 };
