df21a6a2e7d3f895b1b4495b4aa9c9c69a54ec8a
Macish <marcusaxelsson52@gmail.com>
2020-04-06 20:00:25 +0200

    Added style and test values to the card component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./App.css";
 import Header from "./Components/Header";
-import Skill from "./Components/Skill";
+import Card from "./Components/Card";
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      <Skill></Skill>
+      <Card></Card>
     </div>
   );
 }
---
+++ src/Components/Card.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+class Card extends Component {
+  render() {
+    return (
+      <div className="card">
+        <img
+          className="card-pic"
+          src={require("../img/web_development.png")}
+          alt={"Web_development.pic"}
+        ></img>
+        <h2 className="card-h2">Web development</h2>
+        <p className="card-p">
+          HTML5
+          <br style="margin-top: 50px;"></br>
+          CSS3
+          <br></br>
+          JavaScript
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Card;
