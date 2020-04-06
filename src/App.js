32e71aa1394ffabe810d1cbc84a8f68341e39139
Macish <marcusaxelsson52@gmail.com>
2020-04-06 17:21:34 +0200

    Added font, changed layout and added component Skill

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Header from "./Components/Header";
+import Skill from "./Components/Skill";
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Skill></Skill>
     </div>
   );
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,25 +1,18 @@
 import React, { Component } from "react";
-import data from "../mydata";
 
 class Header extends Component {
   render() {
     return (
       <div>
-        <img
-          className="header-logo"
-          src={require("../img/marcus_axelsson_logo.png")}
-          alt={data.name}
-        ></img>
         <div className="start-page">
           <img
             className="start-page-marcus"
             src={require("../img/start_page_marcus.png")}
             alt={"Start page pic"}
           ></img>
-          <h1 className="header-h1">
-            Hi, my name is
-            <br></br>
-            Marcus Axelsson
+          <h1 className="start-page-h1">
+            <div className="greeting-start-page-h1">Hi, my name is</div>
+            <div className="marcus-start-page-h1">Marcus</div>
           </h1>
         </div>
       </div>
